fb84fac1697e055a775aaafea996226f37909967
Makoto Kurihara <makoto-kurihara@pitolick.com>
2026-07-13 21:14:09 +0900

    test(js): providers 共有定数テストを楽天Kobo込みに更新（CI JS/Build 修正）

--- tests/js/Admin/providers.test.js
+++ tests/js/Admin/providers.test.js
@@ -1,10 +1,11 @@
 import { PROVIDER_OPTIONS, CRED_SCHEMAS } from '../../../src/Admin/providers';
 
 describe( 'providers shared constants', () => {
-	test( 'PROVIDER_OPTIONS lists manual and dmm-ebook', () => {
+	test( 'PROVIDER_OPTIONS lists manual, dmm-ebook and rakuten-kobo', () => {
 		expect( PROVIDER_OPTIONS.map( ( o ) => o.value ) ).toEqual( [
 			'manual',
 			'dmm-ebook',
+			'rakuten-kobo',
 		] );
 	} );
 
@@ -13,5 +14,16 @@ describe( 'providers shared constants', () => {
 		expect( CRED_SCHEMAS[ 'dmm-ebook' ] ).toHaveLength( 2 );
 		expect( CRED_SCHEMAS[ 'dmm-ebook' ][ 0 ].key ).toBe( 'api_id' );
 		expect( CRED_SCHEMAS[ 'dmm-ebook' ][ 1 ].key ).toBe( 'affiliate_id' );
+	} );
+
+	test( 'CRED_SCHEMAS: rakuten-kobo has application_id, access_key, affiliate_id, allowed_domain', () => {
+		expect( CRED_SCHEMAS[ 'rakuten-kobo' ] ).toHaveLength( 4 );
+		expect( CRED_SCHEMAS[ 'rakuten-kobo' ].map( ( f ) => f.key ) ).toEqual( [
+			'application_id',
+			'access_key',
+			'affiliate_id',
+			'allowed_domain',
+		] );
+		expect( CRED_SCHEMAS[ 'rakuten-kobo' ][ 3 ].required ).toBe( false );
 	} );
 } );
